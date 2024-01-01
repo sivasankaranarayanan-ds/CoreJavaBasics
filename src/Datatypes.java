7f9a5c9d4ad186997896002aa61f10b62ccf5fde
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-01 16:30:07 +0530

    Datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,39 @@
+public class Datatypes
+{
+    public static void main(String[] args)
+    {
+        //char type - 16 bits
+        char a = 'S';
+
+        //integer type - 32 bits
+        int i = 33;
+
+        //boolean type (true or false)
+        boolean boo = false;
+
+        //byte type (-128 to 127 range, 8 bits)
+        byte b = 7;
+
+        //short type - 16 bits
+        short s = 77;
+
+        //double type - 64 bits
+        double d = 78651.2;
+
+        //float type - 32 bits
+        float f = 2.3429f;
+
+        //long type - 64 bits
+        long l = 631852901;
+
+
+        System.out.println("char: " + a);
+        System.out.println("int: " + i);
+        System.out.println("boolean: " + boo);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("double: " + d);
+        System.out.println("float: " + f);
+        System.out.println("long: " + l);
+    }
+}
