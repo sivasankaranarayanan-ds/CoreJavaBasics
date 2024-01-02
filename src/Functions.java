09e3735fbb41bcf6554f487a3c7d983f6a74cffc
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-02 11:03:10 +0530

    Functions.java

---
+++ src/Functions.java
@@ -0,0 +1,13 @@
+public class Functions
+{
+    static void Employees(String name,int age)
+    {
+        System.out.println("Ename: "+name+ " Eage: "+age);
+    }
+
+    public static void main(String[] args)
+    {
+        Employees("Lokesh",26);
+        Employees("Sundharam",32);
+    }
+}
