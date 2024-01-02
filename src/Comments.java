ea65fd3627d8cca49889146bce46dd6ca5f2d4c3
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-02 13:12:30 +0530

    Comments.java

---
+++ src/Comments.java
@@ -0,0 +1,16 @@
+
+    //If the comment only spans one line, use an inline comment //
+
+    //If the comment spans multiple lines, use a block comment /* */
+
+    //The single line comment
+    //Oops concept --> Inheritance,Polymorphism,Encapsulation,Abstraction
+
+    //Block comment
+    /*
+        Oops concept --> Inheritance,
+                         Polymorphism,
+                         Encapsulation,
+                         Abstraction.
+     */
+
