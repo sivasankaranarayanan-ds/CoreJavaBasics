465fa466cadeaa90460a7025d1fb4dcae16c2cbd
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-02 13:17:33 +0530

    Comments.java

--- src/Comments.java
+++ src/Comments.java
@@ -1,16 +1,26 @@
 
+    //If the comment describes how to use the code, use JavaDoc comments /** */
+
     //If the comment only spans one line, use an inline comment //
 
     //If the comment spans multiple lines, use a block comment /* */
 
-    //The single line comment
+    //The single line comment example
     //Oops concept --> Inheritance,Polymorphism,Encapsulation,Abstraction
 
-    //Block comment
+    //Block comment example
     /*
         Oops concept --> Inheritance,
                          Polymorphism,
                          Encapsulation,
                          Abstraction.
+     */
+
+    //Doc comment example
+    /**
+     * Oops concept --> Inheritance,
+     *                  Polymorphism,
+     *                  Encapsulation,
+     *                  Abstraction.
      */
 
