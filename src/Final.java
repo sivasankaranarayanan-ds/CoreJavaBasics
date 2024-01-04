258e430a429ba4e7c6f8360e017bb4d3e733a561
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-04 10:35:45 +0530

    Final

---
+++ src/Final.java
@@ -0,0 +1,10 @@
+public class Final
+{
+    final static int id = 45;   //final keyword we can't reassign, Once created it won't change the value
+
+    public static void main(String[] args)
+    {
+        System.out.println("I'd: " + id);
+    }
+}
+
