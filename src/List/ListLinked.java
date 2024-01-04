608a86b15333b296c0429456312d8c6c77f32f1a
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-04 22:36:01 +0530

    ListLinked

--- src/List/ListLinked.java
+++ src/List/ListLinked.java
@@ -1,2 +1,22 @@
-package List;public class ListLinked {
+package List;
+import java.util.*;
+//Linkedlist program
+public class ListLinked
+{
+    public static void main(String[] args)
+    {
+        LinkedList<String> l=new LinkedList<String>();
+        l.add("Ravi");
+        l.add("Vijay");
+        l.add("Ravi");
+        l.add("Ajay");
+
+        Iterator<String> a=l.iterator();
+        while(a.hasNext())
+        {
+            System.out.println("Student Name: " + a.next());
+        }
+    }
 }
+
+
