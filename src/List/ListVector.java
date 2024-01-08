f051a009166bff5ccc844a1e8099c9b1234176dd
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-08 15:43:42 +0530

    List

--- src/List/ListVector.java
+++ src/List/ListVector.java
@@ -1,2 +1,23 @@
-package List;public class ListVector {
+package List;
+import java.util.*;
+//Vector program
+public class ListVector
+{
+    public static void main(String args[]) {
+        //Create a vector
+        Vector<String> vec = new Vector<String>();
+        vec.add("Tiger");
+        vec.add("Lion");
+        vec.add("Dog");
+        vec.add("Elephant");
+        vec.add("Rat");
+        vec.add("Cat");
+        vec.add("Deer");
+
+        for (String v1: vec)
+        {
+            System.out.println("Animal are: " + v1);
+        }
+        System.out.println("Animal Size: " + vec.size());
+    }
 }
