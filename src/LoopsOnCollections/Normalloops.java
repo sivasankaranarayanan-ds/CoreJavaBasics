08120311f05964d8699972073bd779f77bcf3b96
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-01-08 12:35:02 +0530

    LoopsOnCollections

--- src/LoopsOnCollections/Normalloops.java
+++ src/LoopsOnCollections/Normalloops.java
@@ -1,6 +1,14 @@
-package Streams;
-
+package LoopsOnCollections;
+import java.util.*;
 public class Normalloops
 {
-
+    public static void main(String[] args)
+    {
+        List<String> fruits = Arrays.asList("apple", "banana", "orange");    //Collection
+        for (int i = 0; i < fruits.size(); i++)                              //using normal loop
+        {
+            fruits.set(i, fruits.get(i).toUpperCase());                      //changing one by one uppercase
+            System.out.println(fruits);
+        }
+    }
 }
--- src/LoopsOnCollections/Streams.java
+++ src/LoopsOnCollections/Streams.java
@@ -1,6 +1,21 @@
-package Streams;
+package LoopsOnCollections;
+import java.util.*;
 
+//Why stream?
+        //T
+        //If you change the existing stream or create the new stream,
+        //it will not affect the list which is you create already the collection.
+        //Once you consume the stream, you can't reuse it then it will close automatically.
+        //So, the advantage is your data is not leakage.
 public class Streams
 {
-    
+    public static void main(String[] args)
+    {
+        List<Integer> nums = Arrays.asList(2,9,3,8,4,7,5);   //Collection
+        nums.stream()
+                .filter(n -> n%2==1)                        //filter the odd numbers
+                .sorted()                                   //and sorted
+                .map(n -> n*2)                              //and double it
+                .forEach(n -> System.out.println(n));       //and print the values through for-each loop.
+    }
 }
